831e509f61c84930a43cf1737c76ce880a59f2b9
amaralf <amaralf12@hotmail.com>
2018-06-09 11:52:42 +0200

    sine_wave second optimization and documentation

--- sine_wave.py
+++ sine_wave.py
@@ -7,23 +7,23 @@ import time
 def get_values():
     """Make a sine and cosine wave and store the relevant values in arrays.
     Input: -
-    Output: array value1, with the cosine wave values. array value2, with the sine wave values.
-            array times, with the measurement times."""
-    # initialization
+    Output: array adc_values, with the readout values from the detector.
+            array times, with the measurement times.
+            new set values for dac1 and dac2"""
+    # initialization of all peripherals
     adc = Adafruit_ADS1x15.ADS1115()
     dac1 = Adafruit_MCP4725.MCP4725()
     dac2 = Adafruit_MCP4725.MCP4725(address=0x63, busnum=1)
-
     dac1.set_voltage(int(2048))
     dac2.set_voltage(int(2048))
     
-    GAIN = 1  # of 2/3, afhankelijk van de range
-    delta = 0
-    times = []
+    GAIN = 1                        # of 2/3, dependent on the desired range
+    times = []                      # create some arrays
     adc_values = []
     sine = []
     cosine = []
 
+    # create a sine and cosine wave for sampling
     for i in range(2000):
         sine.append(int(2048 + 2048*math.sin(0.001*math.pi*i)))
         cosine.append(int(2048 + 2048*math.cos(0.001*math.pi*i)))
@@ -32,14 +32,16 @@ def get_values():
         cosine.append(int(2048))
 
     start_time = time.time()
+    delta = 0
     while delta <= 0.2:
         delta = time.time() - start_time
         times.append(delta)
-        dac1.set_voltage(cosine[int(10000*delta)])
+        dac1.set_voltage(cosine[int(10000*delta)])      # sample values from waves according to delta
         dac2.set_voltage(sine[int(10000*delta)])
-        adc_values.append(adc.read_adc(3, gain=GAIN))
+        adc_values.append(adc.read_adc(3, gain=GAIN))   # read the adc and store values in array
 
     dac1.set_voltage(int(2048))
     dac2.set_voltage(int(2048))
     print(times)
     print(len(adc_values))
+    print(adc_values)
