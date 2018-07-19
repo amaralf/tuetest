ada7f8f4f77fbb73084996c38db34c3b57e3a221
R. Schmidt <amaralf12@hotmail.com>
2018-07-19 11:37:58 +0200

    Rework

--- fourier.py
+++ fourier.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.fftpack import fft
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
 def load_test_data():
@@ -60,57 +60,57 @@ def test_data_fourier(data):
     return fourierx, fouriery
 
 
-def plot(y):
-    x = np.linspace(0, 20, 20)
-    plt.plot(x, y)
-    # Give a title for the sine wave plot
-    plt.title('Fourier')
-    # Give x axis label for the sine wave plot
-    plt.xlabel('Number of pulses')
-    # Give y axis label for the sine wave plot
-    plt.ylabel('Amplitude = sin(time)')
-    plt.grid(True, which='both')
-    plt.axhline(y=0, color='k')
-    plt.show()
+# def plot(y):
+#     x = np.linspace(0, 20, 20)
+#     plt.plot(x, y)
+#     # Give a title for the sine wave plot
+#     plt.title('Fourier')
+#     # Give x axis label for the sine wave plot
+#     plt.xlabel('Number of pulses')
+#     # Give y axis label for the sine wave plot
+#     plt.ylabel('Amplitude = sin(time)')
+#     plt.grid(True, which='both')
+#     plt.axhline(y=0, color='k')
+#     plt.show()
 
 
-def make_sine():
-    # Get x values of the sine wave
-    time = np.linspace(0, 0.2, 20)
-    frequency = 10  # hertz
-    w = frequency*(2*np.pi)
-    # Amplitude of the sine wave is sine of a variable like time
-    amplitude = np.sin(w*time)
-    # Plot a sine wave using time and amplitude obtained for the sine wave
-    plt.plot(time, amplitude)
-    # Give a title for the sine wave plot
-    plt.title('Sine wave')
-    # Give x axis label for the sine wave plot
-    plt.xlabel('Time')
-    # Give y axis label for the sine wave plot
-    plt.ylabel('Amplitude = sin(time)')
-    plt.grid(True, which='both')
-    plt.axhline(y=0, color='k')
-    plt.show()
-    return time, amplitude
+# def make_sine():
+#     # Get x values of the sine wave
+#     time = np.linspace(0, 0.2, 20)
+#     frequency = 10  # hertz
+#     w = frequency*(2*np.pi)
+#     # Amplitude of the sine wave is sine of a variable like time
+#     amplitude = np.sin(w*time)
+#     # Plot a sine wave using time and amplitude obtained for the sine wave
+#     plt.plot(time, amplitude)
+#     # Give a title for the sine wave plot
+#     plt.title('Sine wave')
+#     # Give x axis label for the sine wave plot
+#     plt.xlabel('Time')
+#     # Give y axis label for the sine wave plot
+#     plt.ylabel('Amplitude = sin(time)')
+#     plt.grid(True, which='both')
+#     plt.axhline(y=0, color='k')
+#     plt.show()
+#     return time, amplitude
 
 
-def initialize():
-    time1 = [1.2159347534179688e-05, 0.01201176643371582, 0.023653745651245117, 0.035660505294799805, 0.048452138900756836, 0.06030440330505371, 0.07195305824279785, 0.08367443084716797, 0.09540987014770508, 0.10707855224609375, 0.11869525909423828, 0.13054752349853516, 0.14247393608093262, 0.15423583984375, 0.1658763885498047, 0.17781972885131836, 0.18955039978027344, 0.20127224922180176]
-    wave1 = [14365, 10491, 11621, 17371, 25038, 29436, 29013, 23811, 16719, 11330, 10312, 14351, 21343, 27454, 29431, 26975, 20539, 13622]
-    time2 = [8.821487426757812e-06, 0.011968612670898438, 0.023641109466552734, 0.03393983840942383, 0.04433846473693848, 0.05458998680114746, 0.06473612785339355, 0.07487940788269043, 0.0850977897644043, 0.09535956382751465, 0.10550093650817871, 0.11561751365661621, 0.12573027610778809, 0.13593220710754395, 0.1462996006011963, 0.1565103530883789, 0.16662931442260742, 0.17674732208251953, 0.18695783615112305, 0.19711709022521973, 0.20723867416381836]
-    wave2 = [12206, 10350, 13094, 18703, 24937, 29093, 29679, 26430, 20558, 14560, 10703, 10451, 13896, 19749, 25772, 28853, 29048, 25583, 19671, 13941, 10388]
-    time3 = [1.4543533325195312e-05, 0.012311935424804688, 0.023920059204101562, 0.034155845642089844, 0.04441499710083008, 0.05466270446777344, 0.06482958793640137, 0.07497477531433105, 0.08509325981140137, 0.09536337852478027, 0.10574102401733398, 0.11623549461364746, 0.126786470413208, 0.1370389461517334, 0.14725852012634277, 0.15747499465942383, 0.1676163673400879, 0.17786288261413574, 0.18806052207946777, 0.19827866554260254, 0.2084972858428955]
-    wave3 = [23424, 28932, 29658, 26652, 20942, 14930, 10976, 10512, 13778, 19717, 25674, 28929, 28807, 25033, 18946, 13320, 10335, 11218, 15495, 21628, 26946]
-    graph = list(zip(time2, wave2));
-    graph = sorted(graph, key=lambda l:l[0])
-    plt.xlabel('Time in seconds')
-    plt.ylabel('Wave')
-    x = [i[0] for i in graph]
-    y = [i[1] for i in graph]
-    plt.plot(x, y, marker='o')
-    plt.show()
-    return time2, wave2
+# def initialize():
+#     time1 = [1.2159347534179688e-05, 0.01201176643371582, 0.023653745651245117, 0.035660505294799805, 0.048452138900756836, 0.06030440330505371, 0.07195305824279785, 0.08367443084716797, 0.09540987014770508, 0.10707855224609375, 0.11869525909423828, 0.13054752349853516, 0.14247393608093262, 0.15423583984375, 0.1658763885498047, 0.17781972885131836, 0.18955039978027344, 0.20127224922180176]
+#     wave1 = [14365, 10491, 11621, 17371, 25038, 29436, 29013, 23811, 16719, 11330, 10312, 14351, 21343, 27454, 29431, 26975, 20539, 13622]
+#     time2 = [8.821487426757812e-06, 0.011968612670898438, 0.023641109466552734, 0.03393983840942383, 0.04433846473693848, 0.05458998680114746, 0.06473612785339355, 0.07487940788269043, 0.0850977897644043, 0.09535956382751465, 0.10550093650817871, 0.11561751365661621, 0.12573027610778809, 0.13593220710754395, 0.1462996006011963, 0.1565103530883789, 0.16662931442260742, 0.17674732208251953, 0.18695783615112305, 0.19711709022521973, 0.20723867416381836]
+#     wave2 = [12206, 10350, 13094, 18703, 24937, 29093, 29679, 26430, 20558, 14560, 10703, 10451, 13896, 19749, 25772, 28853, 29048, 25583, 19671, 13941, 10388]
+#     time3 = [1.4543533325195312e-05, 0.012311935424804688, 0.023920059204101562, 0.034155845642089844, 0.04441499710083008, 0.05466270446777344, 0.06482958793640137, 0.07497477531433105, 0.08509325981140137, 0.09536337852478027, 0.10574102401733398, 0.11623549461364746, 0.126786470413208, 0.1370389461517334, 0.14725852012634277, 0.15747499465942383, 0.1676163673400879, 0.17786288261413574, 0.18806052207946777, 0.19827866554260254, 0.2084972858428955]
+#     wave3 = [23424, 28932, 29658, 26652, 20942, 14930, 10976, 10512, 13778, 19717, 25674, 28929, 28807, 25033, 18946, 13320, 10335, 11218, 15495, 21628, 26946]
+#     graph = list(zip(time2, wave2));
+#     graph = sorted(graph, key=lambda l:l[0])
+#     plt.xlabel('Time in seconds')
+#     plt.ylabel('Wave')
+#     x = [i[0] for i in graph]
+#     y = [i[1] for i in graph]
+#     plt.plot(x, y, marker='o')
+#     plt.show()2
+#     return time2, wave2
 
 
 def fourier(time, amplitude):
@@ -119,11 +119,11 @@ def fourier(time, amplitude):
     yf = fft(amplitude)  # fourier transform
     hzvals = np.linspace(0.0, 1.0/(2.0*T), N/2)
     amplitudes = 2.0/N * np.abs(yf[:N//2])
-    fig, ax = plt.subplots()
-    ax.plot(hzvals, amplitudes)
-    ax.grid(True)
-    plt.xlabel('Hz')
-    plt.show()
+    # fig, ax = plt.subplots()
+    # ax.plot(hzvals, amplitudes)
+    # ax.grid(True)
+    # plt.xlabel('Hz')
+    # plt.show()
     return hzvals, amplitudes
 
 
