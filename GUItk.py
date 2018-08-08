6691ee36c296ae49f6edde28b8a3caf16c1dadc3
Tom Franken <tom.franken311@gmail.com>
2018-08-08 10:31:12 +0200

    just an update

--- GUItk.py
+++ GUItk.py
@@ -458,6 +458,7 @@ class App:
         if len(measurements) != 20:
             print("more than 20 measurements")
         halflength = int(len(measurements)/2)
+        print(halflength + "should be 10")
         avg1 = sum(measurements[:halflength])/halflength
         avg2 = sum(measurements[halflength+1:])/halflength
         res = self.getResult(avg)
