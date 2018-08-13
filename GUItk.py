682506f5692e0597b174601befc53e548c5eb205
Tom Franken <tom.franken311@gmail.com>
2018-08-13 12:51:59 +0200

    added scrollbar 8

--- GUItk.py
+++ GUItk.py
@@ -489,13 +489,14 @@ class App:
             filetext = "There are no results yet for patient " + str(self.patient_id) + "."
         else:
             filetext = file.read()
-        filelabel = t.Canvas(fileframe, bg=self.color3)
-        filelabel.create_text(0, 0, anchor="nw", font=(self.font, self.normalfontsize),
+        filecanvas = t.Canvas(fileframe, bg=self.color3)
+        filecanvas.place(relheight=1, relwidth=0.9, relx=0, rely=0)
+        filelabel=t.Label(filecanvas, anchor="nw", font=(self.font, self.normalfontsize),
                               text=filetext)
-        filelabel.place(relheight=1, relwidth=0.95, relx=0, rely=0)
-        filescroll = t.Scrollbar(fileframe, command=filelabel.yview)
-        filelabel.config(yscrollcommand=filescroll.set)
-        filescroll.place(relheight=1, relwidth=0.05, relx=0.9, rely=0)
+        filelabel.place()
+        filescroll = t.Scrollbar(fileframe, command=filecanvas.yview)
+        filecanvas.config(yscrollcommand=filescroll.set)
+        filescroll.place(relheight=1, relwidth=0.1, relx=0.9, rely=0)
         fileframe.update()
 
     def stop(self, *args):
